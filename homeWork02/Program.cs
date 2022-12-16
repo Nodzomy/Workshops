948cefb4ef39df26a7030a7af1b1b903a0c0c6bf
«Nodzomy» <>
2022-12-16 13:15:53 +0300

    add HomeWork02

---
+++ homeWork02/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"{a} > {b} !");
+}
+else
+{
+    Console.WriteLine($"{b} > {a} !");
+}
