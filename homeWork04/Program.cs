7fc8cf7ede4228b18508a7fc327485a077cbe684
«Nodzomy» <>
2022-12-16 14:40:54 +0300

    Add Home Work 04

---
+++ homeWork04/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max;
+
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+
+}
+
+if(max > c)
+{
+    max = max;
+}
+else
+{
+    max = c;
+}
+Console.WriteLine($"Ниабольшее {max} !");
