f1d49a0cdd20f84f313dc3c26f6b8351e8ee644c
«Nodzomy» <>
2022-12-16 12:42:39 +0300

    Add Example 001

--- Exmpl_001/Program.cs
+++ Exmpl_001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая принимает число на вход и выдает его квадрат
+// (число умноеженное на само себя)
+
+Console.WriteLine("Введите число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int result = namber * namber;
+
+Console.WriteLine($"Квадрат числа {namber} будет {result}");
+
