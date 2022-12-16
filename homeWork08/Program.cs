2eaffbddd045e5be7146aff33331b285bd57bd66
«Nodzomy» <>
2022-12-16 16:09:38 +0300

    Aadd Home Work 08

---
+++ homeWork08/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= n)
+
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i},");
+    }
+    i++;
+}
