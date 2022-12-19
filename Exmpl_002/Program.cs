d12c613d26c2ae2d262b3a9b16f72b2067b162d6
«Nodzomy» <>
2022-12-19 10:45:47 +0300

    Add Exple 002

---
+++ Exmpl_002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное 
+// число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+
+int namber = new Random.Next(10, 100);
+Console.Writeline(namber);
+
+// if (namber /10 > namber % 10) Console.Writeline(namber /10);
+// else Console.Writeline(namber%10);
+
+Console.WriteLine(namber / 10 > namber % 10 ? namber / 10 : namber % 10);
--- homeWork06/Program.cs
+++ homeWork06/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите число ");
 
-double a = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(a % 2 == 0 ? "Четное" : "Не четное");
 
